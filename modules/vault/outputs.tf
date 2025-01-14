5fbe4238bb1997546a74834b99d5059bdd96f6a3
Vinay Kudari <vinay@braveorbit.io>
2025-01-13 17:23:27 -0800

    Fix bugs

--- modules/vault/outputs.tf
+++ modules/vault/outputs.tf
@@ -6,14 +6,6 @@ output "vault_service_url" {
   value = google_cloud_run_service.vault_service.status[0].url
 }
 
-output "vector_index_name" {
-  value = google_vertex_ai_index.vector_index.display_name
-}
-
-output "vector_endpoint_name" {
-  value = google_vertex_ai_index_endpoint.vector_endpoint.display_name
-}
-
 output "firestore_database_name" {
   value = google_firestore_database.default.name
 }
